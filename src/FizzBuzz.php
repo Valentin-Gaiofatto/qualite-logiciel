2555789261983fad25bf0e97ab71934e29a67e48
ValentinDynoco <valentin@dynoco.fr>
2022-01-26 14:19:55 +0100

    third commit

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,12 +6,15 @@ class FizzBuzz
         $res = $int;
         if($int % 3 == 0 && $int % 5 == 0) {
             $res = "FizzBuzz";
+            return ;
         }
         else if($int % 3 == 0) {
             $res = "Fizz";
+            return ;
         }
         else if ($int % 5 == 0) {
             $res = "Buzz";
+            return;
         }
         return $res;
     }
